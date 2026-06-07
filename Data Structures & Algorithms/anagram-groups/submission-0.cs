92c99127ad480ec5a48719a9d315efa811cc76a0
Mohamed Morad <133556068+mo7morad@users.noreply.github.com>
2026-06-07 17:31:50 +0800

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        List<List<string>> values = new List<List<string>>();
+        
+        for (int i = 0; i < strs.Length; i++) {
+            bool grouped = false;
+            
+            for (int j = 0; j < values.Count; j++) {
+                // compare against the first item in the group.
+                if (IsAnagram(strs[i], values[j][0])) {
+                    values[j].Add(strs[i]); // belongs here
+                    grouped = true;
+                    break;
+                }
+            }
+            
+            if (!grouped) {
+                // no existing group matched — start a new one
+                values.Add(new List<string> { strs[i] });
+            }
+        }
+        
+        return values;
+    }
+
+    public bool IsAnagram(string s, string t) {
+        if (s.Length != t.Length) return false;
+        
+        Dictionary<char, int> charNCount = new Dictionary<char, int>();
+        
+        foreach (char c in s) {
+            if (charNCount.ContainsKey(c)) charNCount[c]++;
+            else charNCount[c] = 1;
+        }
+        foreach (char c in t) {
+            if (!charNCount.ContainsKey(c)) return false;
+            charNCount[c]--;
+            if (charNCount[c] < 0) return false;
+        }
+        return true;
+    }
+}
